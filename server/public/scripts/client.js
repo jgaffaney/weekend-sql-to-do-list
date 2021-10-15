d748ceadf49f8c0523d4e5b0f515bdddf43f3507
John Gaffaney <87045628+jgaffaney@users.noreply.github.com>
2021-10-15 16:48:37 -0500

    initial render complete, all data to dueTodayBody

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,22 +4,39 @@ $(readyNow);
 
 function readyNow() {
     console.log('JQ');
-    $('#displayList').on('click', displayList)
+    // initial display
+    displayList();
+
 }
 
-function render() {
+// function to render to the DOM
+function render(todo) {
+    let el = $('#dueTodayBody');
+    el.empty();
+    for(item of todo) {
+        let inputText = `
+            <tr>
+                <td>${item.task}</td>
+                <td>${item.priority}</td>
+                <td>${item.due_date}</td>
+                <td><button id="markCompletedBtn">Mark Completed?</button></td>
+        `
+        el.append(inputText);
+    }
+
     console.log('in render');
 }
 
+//Function to trigger a GET request for table data
 function displayList() {
     console.log('in display');
     
     $.ajax({
         method: 'GET',
         url: '/todo'
-    }).then((req, res) => {
+    }).then((res) => {
         console.log('response from server: ', res);
-        render();
+        render(res);
     }).catch((err) => {
         console.log('Error in retrieving from DB', err);
         res.sendStatus(500);
